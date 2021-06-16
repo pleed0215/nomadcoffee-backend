a2132c467f289069bf4b547383759bd7b4a126a0
Lee Eun Deok <pleed0215@gmail.com>
2021-06-16 14:58:52 +0900

    Now, PAGE_SIZE env can control whole page size

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
-export const PAGE_SIZE = 10;
+export const PAGE_SIZE = process.env.PAGE_SIZE ? +process.env.PAGE_SIZE : 10;
